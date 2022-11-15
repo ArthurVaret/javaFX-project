0ae2534dcbcd98cfa192153aed3be32cbd39ef92
strawhattom <xie.tom7@gmail.com>
2022-11-15 10:47:50 +0100

    fix : sql population

--- src/main/java/service/population.sql
+++ src/main/java/service/population.sql
@@ -1,3 +1,3 @@
-insert into products(name,type,price,cost,stock,size) values ("Air Force 1", "shoe", 100, 20, 7, 42);
-insert into products(name,type,price,cost,stock,size) values ("T-Shirt Balenciaga", "cloth", 130, 30, 13, 46);
-insert into products(name,type,price,cost,stock) values ("Rolex", "accessory", 10999, 6000, 2);
+insert into products(name,type,price,cost,stock,size) values ("Air Force 1", "Shoe", 100, 20, 7, 42);
+insert into products(name,type,price,cost,stock,size) values ("T-Shirt Balenciaga", "Cloth", 130, 30, 13, 46);
+insert into products(name,type,price,cost,stock) values ("Rolex", "Accessory", 10999, 6000, 2);
